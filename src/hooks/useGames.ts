c38421a78ce617cc040441aed723cd0db1bb4556
youssefash97 <youssef.ashour71@hotmail.com>
2024-03-18 17:37:20 +0000

    show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,21 +23,27 @@ export interface Game {
 const useGames = ()=>{
     const [games, setGames] = useState<Game[]>([]);
     const [error, seterror] = useState("");
+    const [isLoading,setLoading]=useState(false)
   
     useEffect(() => {
 const controller = new AbortController();
-
+setLoading(true)
       apiClient
         .get<FetchGamesResponse>("/games",{signal:controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+            setGames(res.data.results)
+            setLoading(false)
+        })
         .catch((err) => {
             if (err instanceof CanceledError) return;
-            seterror(err.message)});
+            seterror(err.message)
+            setLoading(false)
+        });
 
         return()=> controller.abort()
     },[]);
 
-    return{games,error}
+    return{games,error,isLoading}
   
 }
 
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,5 @@
-import noImage from '../assets/no-image-placeholder.webp';
 
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return noImage;
   
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
