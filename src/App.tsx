312f156743d83a912fc22b1a8c64bb5f28e0a0f0
youssefash97 <youssef.ashour71@hotmail.com>
2024-03-18 14:52:34 +0000

    Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 function App() {
-  <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav "" main"`,
+        lg: `"nav nav""aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar></NavBar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
