21ad23875ac617cfd519e17b411e8b0e9510dedb
youssefash97 <youssef.ashour71@hotmail.com>
2024-03-20 12:47:17 +0000

    implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,6 +16,6 @@ export interface Game {
   }
   
 
-const useGames = (gameQuery:GameQuery)=> useData<Game>("/games", {params: {genres:gameQuery.genre?.id, platforms: gameQuery.platform?.id,ordering: gameQuery.sortOrder }}, [gameQuery]);
+const useGames = (gameQuery:GameQuery)=> useData<Game>("/games", {params: {genres:gameQuery.genre?.id, platforms: gameQuery.platform?.id,ordering: gameQuery.sortOrder, search: gameQuery.searchText }}, [gameQuery]);
 
 export default useGames;
